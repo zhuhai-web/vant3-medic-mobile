3b4f8cb9fcc25c18ba9bb9dd8c416a62278e7fec
huzongyao <huzongyao@tangbull.com>
2022-08-18 18:21:36 +0800

    fix title

--- src/view/main/TabHome.vue
+++ src/view/main/TabHome.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-    <van-nav-bar title="标题" placeholder="true" fixed/>
+    <van-nav-bar title="标题" :placeholder="true" fixed/>
     <div>
       <var-button @click="onBtnClick">默认按钮</var-button>
+      <var-row>
+        <var-col :span="8">span: 8</var-col>
+        <var-col :span="8">span: 8</var-col>
+        <var-col :span="8">span: 8</var-col>
+      </var-row>
     </div>
   </div>
 </template>
