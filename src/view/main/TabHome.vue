19f1546295feb2733083183165a9ed1091b8b465
huzongyao <huzongyao@tangbull.com>
2022-09-19 17:00:05 +0800

    add some demo

--- src/view/main/TabHome.vue
+++ src/view/main/TabHome.vue
@@ -7,6 +7,28 @@
         <var-button type="info">信息按钮</var-button>
         <var-button type="success">成功按钮</var-button>
       </var-space>
+
+      <var-tabs v-model:active="active">
+        <var-tab name="选项1">选项1</var-tab>
+        <var-tab name="选项2">选项2</var-tab>
+        <var-tab name="选项3">选项3</var-tab>
+      </var-tabs>
+
+      <var-skeleton :loading="loading">Loading Data</var-skeleton>
+
+      <var-steps :active="active">
+        <var-step>Step1</var-step>
+        <var-step>Step2</var-step>
+        <var-step>Step3</var-step>
+        <var-step>Step4</var-step>
+      </var-steps>
+
+      <var-card
+        title="Dangerous"
+        description="The way she came into the place I knew right then and there.There was something different about this girl.The way she moved her hair her face her lines.Divinity in motion as she stalked the room.I could feel the aura of her presence.Every head turned feeling passion and lust.The girl was persuasive the girl I could not trust.The girl was bad.The girl was dangerous."
+      />
+
+      <var-divider description="Description" />
     </div>
   </div>
 </template>
@@ -14,7 +36,11 @@
 <script>
   export default {
     name: "TabHome",
-    mounted() {
+    data() {
+      return {
+        loading: true,
+        active: '',
+      };
     },
     methods: {},
   }
